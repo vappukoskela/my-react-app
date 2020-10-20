01384e7ab3899922f8723e3b1976fc846c4e1e4a
vappukoskela <vappu.koskela@gmail.com>
2020-10-20 14:17:25 +0300

    Verolaskuri ilman nappia

--- src/App.js
+++ src/App.js
@@ -8,29 +8,36 @@ function App() {
   // array destructuring
   const [bruttopalkka, setBruttopalkka] = useState(0);
   const [veroprosentti, setVeroprosentti] = useState(0);
-  const [veromäärä, setVeromäärä] = useState();
+  const [veromäärä, setVeromäärä] = useState(0);
   // const [teksti, setTeksti] = useState("");
 
 
   // poista nappula -> laskee automaattisesti kun palkkaa muutetaan
-  const nappiaPainettu=() => {
+/*  const nappiaPainettu=() => {
     let vero = bruttopalkka*veroprosentti/100;
     setVeromäärä(vero);
   }
+*/  
   const palkkaMuuttunut=(event) => {
     setBruttopalkka(event.target.value);
+    laskeVero();
   }
   const veroprosMuuttunut=(event) => {
     setVeroprosentti(event.target.value);
+    laskeVero();
+  }
+  const laskeVero=() => {
+    let vero = bruttopalkka*veroprosentti/100;
+    setVeromäärä(vero);
   }
 
   // JSX
   return (
     <div>
-      <button onClick={nappiaPainettu}>Laske veron määrä</button>
+    {/*  <button onClick={nappiaPainettu}>Laske veron määrä</button>*/}
       <input onChange={(event)=>palkkaMuuttunut(event)} value={bruttopalkka}></input>
       <input onChange={(event)=>veroprosMuuttunut(event)} value={veroprosentti}></input>
-      <p>{veromäärä}</p>
+      <p>Veromäärä: <b>{veromäärä}</b></p>
     </div>
   );
 }
