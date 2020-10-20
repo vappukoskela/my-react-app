b71c30be13c36154f0336771221cc5ae4908f15a
vappukoskela <vappu.koskela@gmail.com>
2020-10-20 15:13:41 +0300

    Ratkaisu käyttäen useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
 
@@ -9,24 +9,16 @@ function App() {
   const [bruttopalkka, setBruttopalkka] = useState(0);
   const [veroprosentti, setVeroprosentti] = useState(0);
   const [veromäärä, setVeromäärä] = useState(0);
-  // const [teksti, setTeksti] = useState("");
-
-
-  // poista nappula -> laskee automaattisesti kun palkkaa muutetaan
-/*  const nappiaPainettu=() => {
+  useEffect(()=>{
     let vero = bruttopalkka*veroprosentti/100;
     setVeromäärä(vero);
-  }
-*/  
+  },[bruttopalkka, veroprosentti]);
+
   const palkkaMuuttunut=(event) => {
     setBruttopalkka(event.target.value);
   }
   const veroprosMuuttunut=(event) => {
     setVeroprosentti(event.target.value);
-  }
-  const laskeVero=() => {
-    let vero = bruttopalkka*veroprosentti/100;
-    return vero;
   }
 
   // JSX
@@ -35,7 +27,7 @@ function App() {
     {/*  <button onClick={nappiaPainettu}>Laske veron määrä</button>*/}
       <input onChange={(event)=>palkkaMuuttunut(event)} value={bruttopalkka}></input> eur <br></br>
       <input onChange={(event)=>veroprosMuuttunut(event)} value={veroprosentti}></input> %
-      <p>Veromäärä: <b>{laskeVero()}</b></p>
+      <p>Veromäärä: <b>{veromäärä}</b></p>
     </div>
   );
 }
